5e8421faf2d9cf653a71d0c30f817dc17c5a5de6
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-09-10 17:02:21 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackboneTaskLists::Application.config.secret_key_base = '598644a8950876f0d11f83e09307e65fd708093797de123ecec37322e3d767a7d8e7a83dd503417ebed5d6939626a0344e0700b01e388e24edde9c56a486d5b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BackboneTaskLists::Application.config.session_store :cookie_store, key: '_backbone_task_lists_session'
