033755f244a45cc095ab8f5e1a5e6edafa5e9518
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-09-14 19:19:16 -0700

    remove task router show action, modify page headers

--- app/assets/javascripts/routers/tasks_router.js
+++ app/assets/javascripts/routers/tasks_router.js
@@ -1,18 +1,13 @@
 var TaskRouter = Backbone.Router.extend({
 	
 	routes: {
-		"": 					"index",  //#root to list of all tasks for now
+		"tasks": 					"index",  //#root to list of all tasks for now
 	},
 
 	index: function() {
 		all_tasks = new TaskList
 		var view = new TaskListView({collection: all_tasks})
 		all_tasks.fetch({reset: true})
-	},
-
-	show: function() {
-
 	}
-
 
 });
